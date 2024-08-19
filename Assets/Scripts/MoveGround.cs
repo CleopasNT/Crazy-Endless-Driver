6a34ebf8db8cf775e1295bc3143e0bcfbded60c0
Cleopas Noel <cleopasnoel@outlook.com>
2024-08-19 19:29:23 -0400

    Made ground move
    Made ground move and repeat

--- Assets/Scripts/MoveGround.cs
+++ Assets/Scripts/MoveGround.cs
@@ -11,22 +11,35 @@ public class MoveGround : MonoBehaviour
     private float repeatWidth;
     public float speed = 5.0f;
     public float zDestroy = -30;
+    public bool isDestroyed = false;
 
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
-        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
+        repeatWidth = GetComponent<BoxCollider>().size.z / 2;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+
+        RepeatWidth();
 
         if (transform.position.z < zDestroy)
         {
             Destroy(gameObject);
+            isDestroyed = true;
+        }
+    }
+
+    public void RepeatWidth()
+    {
+        if (transform.position.z < startPos.z - repeatWidth)
+        {
+            transform.position = startPos;
         }
     }
 }
